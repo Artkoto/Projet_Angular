26d79185fdd4b92dd6cf08e9e6b5d6f487532276
Artkoto <arnaudakoto@live.fr>
2020-12-30 00:27:28 +0100

    debut formation angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false ;
+  appareils = [
+    {
+      name : "Télévision",
+      status : "éteint"
+    },
+    {
+      name : "Macchine à laver",
+    status : "allumé"
+  },
+  {
+    name : "Ordinateur",
+    status : "éteint"
+  }
+  ];
+
+  constructor(){
+    setTimeout(
+      () => {
+        this.isAuth = true ;
+      }, 4000 
+    );
+  }
+  onAllumer(){
+    console.log('On allume tout');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
