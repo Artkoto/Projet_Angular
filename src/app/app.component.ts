27b26b429675b4aea9ab4d506d98a5cef93c5e3c
Artkoto <arnaudakoto@live.fr>
2020-12-30 01:30:12 +0100

    remise à zero pour exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { promise } from 'protractor';
-
 
 @Component({
   selector: 'app-root',
@@ -8,42 +6,6 @@ import { promise } from 'protractor';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isAuth = false ;
-  lastUpdate = new Promise(
-      (resolve , reject) =>{
-        const date = new Date();
-        setTimeout(
-          ()=> {
-            resolve (date); 
-
-          },
-          4000
-        )
-      }
-  );
-  appareils = [
-    {
-      name : "Télévision",
-      status : "éteint"
-    },
-    {
-      name : "Macchine à laver",
-    status : "allumé"
-  },
-  {
-    name : "Ordinateur",
-    status : "éteint"
-  }
-  ];
-
   constructor(){
-    setTimeout(
-      () => {
-        this.isAuth = true ;
-      }, 4000 
-    );
-  }
-  onAllumer(){
-    console.log('On allume tout');
   }
 }
